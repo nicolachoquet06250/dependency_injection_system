0df7c2e3bea07b7d8bcb02f9162e645d16181766
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-07-24 13:20:32 +0200

    add htaccess for cleanup urls

--- demo/classes/controllers/api/ControllerAPI1.php
+++ demo/classes/controllers/api/ControllerAPI1.php
@@ -8,6 +8,18 @@ use mvc_router\mvc\Controller;
 use mvc_router\router\Router;
 
 class ControllerAPI1 extends Controller {
+
+	public function error404($message = 'Page not found !') {
+		header('HTTP/1.0 404 '.$message);
+		exit($this->inject->get_service_json()->encode(
+			[
+				'error' => true,
+				'status' => 404,
+				'message' => $message,
+			]
+		));
+	}
+
 	/**
 	 * @route \/api\/user\/([a-zA-Z0-9\-\_\+\@]+)
 	 * @param Router $router
