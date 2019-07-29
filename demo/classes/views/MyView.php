055d9193162ca2003768a548e68ecc6e7ecca303
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-07-29 16:29:01 +0200

    add links for routes page

--- demo/classes/views/MyView.php
+++ demo/classes/views/MyView.php
@@ -5,15 +5,29 @@ namespace mvc_router\mvc\views;
 
 
 use mvc_router\mvc\View;
+use mvc_router\services\Translate;
 
 class MyView extends View {
 	/** @var \mvc_router\services\Translate $translate */
 	public $translate;
 
+	protected function __($text, $params = []) {
+		return $this->translate->__($text, $params);
+	}
+
 	public function render() {
 		$lang = $this->translate->get_default_language();
-		return '<html lang="'.$lang.'">
-	<title>coucou</title>
-</html>';
+		return <<<EOT
+<Doctype html>
+<html lang="{$lang}">
+	<head>
+		<meta charset="utf-8" />
+		<title>{$this->__('coucou')}</title>
+	</head>
+	<body>
+		<h2>{$this->__('coucou')}</h2>
+	</body>
+</html>
+EOT;
 	}
 }
